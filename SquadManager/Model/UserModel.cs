f91f7bd2c09a77cb0ebd142099b5e99861143ca1
Guilherme Ytalo <gydaytalo@gmail.com>
2024-02-21 21:59:43 -0300

    first commit

---
+++ SquadManager/Model/UserModel.cs
@@ -0,0 +1,10 @@
+namespace SquadManager.Model;
+
+public class UserModel
+{
+    public string Username { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+}
